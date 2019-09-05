9cc74d05a919e73b06f3557ef3734ded9878094a
Adil Dev <adilbekmomunaliev@Adilbeks-MacBook-Pro.local>
2019-09-05 11:45:05 -0700

    Updates

---
+++ precache-manifest.094e1dde116a22f17be59b7c567a8301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5852136d5e952a56085f1e8462a520f",
+    "url": "/gol/index.html"
+  },
+  {
+    "revision": "c35f1d015e6f020c6858",
+    "url": "/gol/static/css/main.1dfc6f38.chunk.css"
+  },
+  {
+    "revision": "dacc2e49936e04e3b93a",
+    "url": "/gol/static/js/2.88e53fd3.chunk.js"
+  },
+  {
+    "revision": "c35f1d015e6f020c6858",
+    "url": "/gol/static/js/main.76a95d01.chunk.js"
+  },
+  {
+    "revision": "08a9cf1863b9d0f3806b",
+    "url": "/gol/static/js/runtime~main.e7fbfebf.js"
+  }
+]);
